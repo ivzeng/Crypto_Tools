b66891968e38c687f9cedef5adf9c037d72bb071
Yuhong Zeng <a1113526380@gmail.com>
2022-10-13 22:54:49 -0400

    added more state-converting functions

--- linearCryptanalysis.py
+++ linearCryptanalysis.py
@@ -3,13 +3,16 @@
 
 ## Cipher Reference:  http://www.engr.mun.ca/~howard/Research/Papers/ldc_tutorial.html
 
-## The program produces the a linear cryptanalysis based on the ##  given a collection of distinct pairs of plaintext and
-##  ciphertext. The plaintexts and ciphertext need to be stored ##  in two files, respectively, one for each line.
+## The program produces the a linear cryptanalysis based on the 
+##  given a collection of distinct pairs of one plaintext and one
+##  ciphertext. The plaintexts and ciphertext need to be stored
+##  in two files, respectively. One text for each line.
 
 ## Enter command like the following to run the program:
 ## "./linearCryptanalysis.py (plaintextsFile) (ciphertextsFile)" 
 
 import sys
+from turtle import back
 
 # check input files
 if len(sys.argv) != 3:
@@ -18,7 +21,7 @@ if len(sys.argv) != 3:
 
 
 # reads input files and stored them into a list of decimal numbers
-def readIn():
+def readIn() -> list: 
     blockPairs = []
     plaintexts, ciphertexts =  open(sys.argv[1], 'r'), open(sys.argv[2], 'r')
     nextline = plaintexts.readline()
@@ -27,4 +30,36 @@ def readIn():
         nextline = plaintexts.readline()
     return blockPairs
 
-print(readIn())
+# creates a list indicating the both directions of a substitution box
+#   (or permutation)
+def addReverse(forward) -> list:
+    backward =  [-1 for i in range(len(forward))]
+    for i in range(len(forward)):
+        backward[forward[i]] = i
+    return [forward, backward]
+
+# undo the substitution once 
+#   dir =   0 - forward    1 - backward
+def subOnce(txt, sBox, sBoxCount, dir):
+    power = len(sBox[0])
+    res = 0
+    for order in range(sBoxCount):
+        res += (sBox[dir][txt%power])*(power**order)
+        txt //= power
+    return res
+
+
+
+
+# solve
+sBoxSize = 4    # number of bit of the input for a s-box
+sBoxCount = 4   # number of sub-block in each block of text
+blockPairs = readIn()
+sBox = addReverse([14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7])
+
+#print(blockPairs)
+print(sBox)
+print(bin(subOnce(0b0000000100100011, sBox, sBoxCount, 1)))
+
+# for i in range(pow(2,5)):
+#    print(bin(blockPairs[-1][-1] & i))
